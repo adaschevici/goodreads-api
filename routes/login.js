b0c08951496430f4ab43f6b77fc900e264e60590
Artur Daschevici <a.daschevici@gmail.com>
2021-01-22 17:13:58 +0100

    Add login route

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log('Hahah')
+  res.render('account/login', { title: 'Express' });
 });
 
 module.exports = router;
