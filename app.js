daf4e07deae2c0ad7f4109a5e43640645d4fcb7d
simonaco <simona.cotin@gmail.com>
2021-01-23 17:58:43 +0100

    add dummy data

--- app.js
+++ app.js
@@ -18,18 +18,79 @@ app.use(cookieParser());
 app.use("/", indexRouter);
 app.use("/auth", userRouter);
 app.use("/api/meta", (req, res, next) => {
-  res.send("from meta");
-  console.log("bla");
+  res.send([
+    {
+      id: 9780439023480,
+      isbn: "439023483",
+      isbn13: "9780439023480",
+      authors: "Suzanne Collins",
+      year: 2008,
+      title: "The Hunger Games",
+      description:
+        "Voluptatem aut sit consequatur accusantium perferendis. Perferendis sit voluptatem accusantium aut consequatur. Perferendis aut sit consequatur voluptatem accusantium. Sit consequatur voluptatem aut perferendis accusantium.",
+    },
+    {
+      id: 9780439554930,
+      isbn: "439554934",
+      isbn13: "9780439554930",
+      authors: "J.K. Rowling, Mary GrandPré",
+      year: 1997,
+      title: "Harry Potter and the Philosopher's Stone",
+      description:
+        "Voluptatem sit aut accusantium perferendis consequatur. Aut perferendis consequatur accusantium sit voluptatem. Aut accusantium perferendis voluptatem consequatur sit. Perferendis sit consequatur accusantium voluptatem aut.",
+    },
+  ]);
 });
 
 app.use("/api/ratings", (req, res, next) => {
-  res.send("from meta");
-  console.log("bla");
+  res.send([
+    {
+      id: 9780439023480,
+      rating: 4.34,
+    },
+    {
+      id: 9780439554930,
+      rating: 4.44,
+    },
+    {
+      id: 9780316015840,
+      rating: 3.57,
+    },
+  ]);
 });
 
 app.use("/api/images", (req, res, next) => {
-  res.send("from images");
-  console.log("bla");
+  res.send([
+    {
+      id: 9780439023480,
+      image: "https://images.gr-assets.com/books/1447303603m/2767052.jpg",
+    },
+    {
+      id: 9780439554930,
+      image: "https://images.gr-assets.com/books/1474154022m/3.jpg",
+    },
+    {
+      id: 9780316015840,
+      image: "https://images.gr-assets.com/books/1361039443m/41865.jpg",
+    },
+  ]);
+});
+app.use("/api/book-progress/:username", (req, res, next) => {
+  username = req.params.id;
+  res.send([
+    {
+      id: 9780439023480,
+      progress: 0,
+    },
+    {
+      id: 9780439554930,
+      progress: 0,
+    },
+    {
+      id: 9780316015840,
+      progress: 0,
+    },
+  ]);
 });
 
 // catch 404 and forward to error handler
