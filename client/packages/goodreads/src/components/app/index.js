62e01e3527bb7c761bd032adc2e261ae5824cacf
simonaco <simona.cotin@gmail.com>
2021-01-24 19:34:21 +0100

    connect authenticated with NavBar

--- client/packages/goodreads/src/components/app/index.js
+++ client/packages/goodreads/src/components/app/index.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { components } from '@goodreads-v2/component-library'
 import './index.css'
 import BookList from '../book-list'
 
 const { NavBar } = components
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <main style={{ height: '70vh' }}>
-        <BookList />
-      </main>
-    </div>
-  )
+class App extends Component {
+  render() {
+    const { authenticated, username } = this.props
+    return (
+      <div className="App">
+        <NavBar authenticated={authenticated} username={username} />
+        <main style={{ height: '70vh' }}>
+          <BookList />
+        </main>
+      </div>
+    )
+  }
 }
 
-export default App
+function mapStateToProps(state) {
+  const { username, error: authError } = state.auth
+  const authenticated = authError === null
+  return {
+    username,
+    authenticated,
+  }
+}
+export default connect(mapStateToProps)(App)
