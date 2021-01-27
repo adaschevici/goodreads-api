3a60b29a46e45a4fa117acad1d16b84a838304cd
Artur Daschevici <a.daschevici@gmail.com>
2021-01-27 11:12:30 +0100

    update after rebase

--- app.js
+++ app.js
@@ -63,7 +63,6 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  console.log(err);
   // render the error page
   res.status(err.status || 500);
   res.json({ msg: "error" });
